2e98aad9da51b9bacce112daf036917a51fab066
Amitha Ajithkumar <ajithkumar.amitha3@gmail.com>
2026-01-29 23:25:36 -0500

    Remove SQL credentials
    Removed sensitive SQL connection details from the file.

--- sqldetails.py
+++ sqldetails.py
@@ -1,7 +1,1 @@
-#Storing sql
-user = 'avnadmin'
-password ='your_password_here'
-host = '  homelib-db-homelib.d.aivencloud.com'
-password = 'your_password_here'
-port = '19643'
-database = 'defaultdb'
+
