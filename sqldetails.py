85367e380ac13d86538b5591dbe8de4012a9aa14
Amitha Ajithkumar <amitha.ajithkumar4@gmail.com>
2026-01-29 23:18:29 -0500

    added sql db connection

---
+++ sqldetails.py
@@ -0,0 +1,7 @@
+#Storing sql
+user = 'avnadmin'
+password ='your_password_here'
+host = '  homelib-db-homelib.d.aivencloud.com'
+password = 'your_password_here'
+port = '19643'
+database = 'defaultdb'
