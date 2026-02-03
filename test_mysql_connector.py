af6cbe39e5867a6902f1b43e14e0b73f28494f71
Amitha Ajithkumar <ajithkumar.amitha3@gmail.com>
2026-02-03 16:25:50 -0500

    remove rendundant test

--- test_mysql_connector.py
+++ test_mysql_connector.py
@@ -1,12 +1,1 @@
-import mysql.connector
 
-print("mysql.connector attributes:")
-print(dir(mysql.connector))
-
-try:
-    # Try to call connect (should not error if environment is correct)
-    print("Trying mysql.connector.connect...")
-    conn = mysql.connector.connect()
-    print("Connect attribute exists.")
-except Exception as e:
-    print(f"Error: {e}")
