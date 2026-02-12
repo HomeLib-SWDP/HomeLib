1b11f18d3b6dbe7fcc49c36bc65827230dcc5c7e
BrenHay <bd.hay119@gmail.com>
2026-02-12 16:57:29 -0500

    Finished functionality for the search of personal lib

--- static/js/lib_search.js
+++ static/js/lib_search.js
@@ -1,6 +1,9 @@
 
 const API_BASE_URL = "http://127.0.0.1:5000";
 
+var currentLibrary = [];
+var filteredLibrary = [];
+
 async function getLibrary()
 {   
     try
@@ -12,7 +15,14 @@ async function getLibrary()
 
         const data = await response.json()
 
-        console.log(data)
+        for(const book of data)
+        {
+            currentLibrary.push(book)
+        }
+
+        filteredLibrary = currentLibrary;
+
+        console.log(currentLibrary)
     }
     catch(err)
     {
@@ -23,3 +33,87 @@ async function getLibrary()
 document.addEventListener("DOMContentLoaded", () => {
   getLibrary();
 });
+
+async function setupAutoComplete(inputId, suggestionsId)
+{
+    const input = document.getElementById(inputId);
+    const suggestionsBox = document.getElementById(suggestionsId);
+
+    input.addEventListener("input", function (){
+        const query = this.value.trim().toLowerCase();
+
+        suggestionsBox.innerHTML = "";
+
+        if(query.length === 0)
+        {
+            suggestionsBox.style.display = "none";
+            return;
+        }
+
+        const matches = currentLibrary.filter(item => {
+            const value = typeof item === "object" ? item.booktitle : item;
+            const author = typeof item === "object" ? item.author : item;
+            return value.toLowerCase().includes(query) || author.toLowerCase().includes(query);
+        })
+
+        if(matches.length === 0)
+        {
+            suggestionsBox.style.display = "none";
+            return;
+        }
+
+        filteredLibrary = matches;
+        
+        matches.forEach(item =>
+        {
+            const value = typeof item === "object" ? item.booktitle : item;
+            const author = typeof item === "object" ? item.author : item;
+
+            const div = document.createElement("div")
+            div.textContent = value + " by: " + author;
+
+            div.addEventListener("click", function () {
+                input.value = value;
+                suggestionsBox.style.display = "none";
+                displayChosenBook(value);
+            });
+
+            suggestionsBox.appendChild(div)
+        }
+        );
+        suggestionsBox.style.display = "block";
+    });
+
+    document.addEventListener("click", function (e) {
+        if (!e.target.closest(".autocomplete-wrapper")) {
+            suggestionsBox.style.display = "none";
+        }
+    });
+
+    console.log("Auto Complete setup!")
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    setupAutoComplete("search", "suggestions");
+});
+
+async function displayChosenBook(bookName)
+{
+    console.log("Display")
+    console.log(bookName)
+    console.log(currentLibrary);
+    for(book of currentLibrary)
+    {
+        if(book.booktitle === bookName)
+        {
+            const div = document.getElementById("displayLib");
+            const title = document.createElement("h4");
+            title.textContent = book.booktitle;
+            const author = document.createElement("h6");
+            author.textContent = book.author
+
+            div.appendChild(title);
+            div.appendChild(author);
+        }
+    }
+}
