717164f0b8e1c558b529ad4f76d6ed899c24d68f
BrenHay <bd.hay119@gmail.com>
2026-02-19 09:24:21 -0500

    Added styles for search dropdown

--- static/js/library.js
+++ static/js/library.js
@@ -1,118 +1,118 @@
-// const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "http://127.0.0.1:5000";
 
-// var currentLibrary = [];
-// var filteredLibrary = [];
+var currentLibrary = [];
+var filteredLibrary = [];
 
-// async function getLibrary()
-// {   
-//     try
-//     {
-//         const response = await fetch(`${API_BASE_URL}/api/books/get_library`);
+async function getLibrary()
+{   
+    try
+    {
+        const response = await fetch(`${API_BASE_URL}/api/books/get_library`);
 
-//         if(!response.ok)
-//             throw new Error(`Error! ${response.status}`)
+        if(!response.ok)
+            throw new Error(`Error! ${response.status}`)
 
-//         const data = await response.json()
+        const data = await response.json()
 
-//         for(const book of data)
-//         {
-//             currentLibrary.push(book)
-//         }
+        for(const book of data)
+        {
+            currentLibrary.push(book)
+        }
 
-//         filteredLibrary = currentLibrary;
+        filteredLibrary = currentLibrary;
 
-//         console.log(currentLibrary)
-//     }
-//     catch(err)
-//     {
-//         console.error("Error fetching library: ", err)
-//     }
-// }
+        console.log(currentLibrary)
+    }
+    catch(err)
+    {
+        console.error("Error fetching library: ", err)
+    }
+}
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   getLibrary();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  getLibrary();
+});
 
-// async function setupAutoComplete(inputId, suggestionsId)
-// {
-//     const input = document.getElementById(inputId);
-//     const suggestionsBox = document.getElementById(suggestionsId);
+async function setupAutoComplete(inputId, suggestionsId)
+{
+    const input = document.getElementById(inputId);
+    const suggestionsBox = document.getElementById(suggestionsId);
 
-//     input.addEventListener("input", function (){
-//         const query = this.value.trim().toLowerCase();
+    input.addEventListener("input", function (){
+        const query = this.value.trim().toLowerCase();
 
-//         suggestionsBox.innerHTML = "";
+        suggestionsBox.innerHTML = "";
 
-//         if(query.length === 0)
-//         {
-//             suggestionsBox.style.display = "none";
-//             return;
-//         }
+        if(query.length === 0)
+        {
+            suggestionsBox.style.display = "none";
+            return;
+        }
 
-//         const matches = currentLibrary.filter(item => {
-//             const value = typeof item === "object" ? item.booktitle : item;
-//             const author = typeof item === "object" ? item.author : item;
-//             return value.toLowerCase().includes(query) || author.toLowerCase().includes(query);
-//         })
+        const matches = currentLibrary.filter(item => {
+            const value = typeof item === "object" ? item.booktitle : item;
+            const author = typeof item === "object" ? item.author : item;
+            return value.toLowerCase().includes(query) || author.toLowerCase().includes(query);
+        })
 
-//         if(matches.length === 0)
-//         {
-//             suggestionsBox.style.display = "none";
-//             return;
-//         }
+        if(matches.length === 0)
+        {
+            suggestionsBox.style.display = "none";
+            return;
+        }
 
-//         filteredLibrary = matches;
+        filteredLibrary = matches;
         
-//         matches.forEach(item =>
-//         {
-//             const value = typeof item === "object" ? item.booktitle : item;
-//             const author = typeof item === "object" ? item.author : item;
+        matches.forEach(item =>
+        {
+            const value = typeof item === "object" ? item.booktitle : item;
+            const author = typeof item === "object" ? item.author : item;
 
-//             const div = document.createElement("div")
-//             div.textContent = value + " by: " + author;
+            const div = document.createElement("div")
+            div.textContent = value + " by: " + author;
 
-//             div.addEventListener("click", function () {
-//                 input.value = value;
-//                 suggestionsBox.style.display = "none";
-//                 displayChosenBook(value);
-//             });
+            div.addEventListener("click", function () {
+                input.value = value;
+                suggestionsBox.style.display = "none";
+                displayChosenBook(value);
+            });
 
-//             suggestionsBox.appendChild(div)
-//         }
-//         );
-//         suggestionsBox.style.display = "block";
-//     });
+            suggestionsBox.appendChild(div)
+        }
+        );
+        suggestionsBox.style.display = "block";
+    });
 
-//     document.addEventListener("click", function (e) {
-//         if (!e.target.closest(".autocomplete-wrapper")) {
-//             suggestionsBox.style.display = "none";
-//         }
-//     });
+    document.addEventListener("click", function (e) {
+        if (!e.target.closest(".autocomplete-wrapper")) {
+            suggestionsBox.style.display = "none";
+        }
+    });
 
-//     console.log("Auto Complete setup!")
-// }
+    console.log("Auto Complete setup!")
+}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     setupAutoComplete("search", "suggestions");
-// });
+document.addEventListener("DOMContentLoaded", function () {
+    setupAutoComplete("search", "suggestions");
+});
 
-// async function displayChosenBook(bookName)
-// {
-//     console.log("Display")
-//     console.log(bookName)
-//     console.log(currentLibrary);
-//     for(book of currentLibrary)
-//     {
-//         if(book.booktitle === bookName)
-//         {
-//             const div = document.getElementById("displayLib");
-//             const title = document.createElement("h4");
-//             title.textContent = book.booktitle;
-//             const author = document.createElement("h6");
-//             author.textContent = book.author
+async function displayChosenBook(bookName)
+{
+    console.log("Display")
+    console.log(bookName)
+    console.log(currentLibrary);
+    for(book of currentLibrary)
+    {
+        if(book.booktitle === bookName)
+        {
+            const div = document.getElementById("displayLib");
+            const title = document.createElement("h4");
+            title.textContent = book.booktitle;
+            const author = document.createElement("h6");
+            author.textContent = book.author
 
-//             div.appendChild(title);
-//             div.appendChild(author);
-//         }
-//     }
-// }
+            div.appendChild(title);
+            div.appendChild(author);
+        }
+    }
+}
