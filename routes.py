c57ab19b175f0d2406349a3bbbf8f10356c36b6a
BrenHay <bd.hay119@gmail.com>
2026-02-19 09:26:03 -0500

    Added get_library to routes

--- routes.py
+++ routes.py
@@ -21,3 +21,9 @@ def add_manual_book():
         return jsonify({"message" : f"'{new_book.title}' added successfully!"}), 201
     return jsonify({"Error": "failed to save book"}), 500
 
+@books_bp.route('/get_library', methods=['GET'])
+def get_library():
+    library = get_books()
+    library_dicts = [book.to_dict() for book in library]
+    return jsonify(library_dicts)
+
