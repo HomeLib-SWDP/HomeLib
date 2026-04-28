06544db10d7e66da7dc27907a7b4c86b3af7d87e
Mar-coder23 <148150802+Mar-coder23@users.noreply.github.com>
2026-04-27 22:29:14 -0400

    alerts for explore and library

--- static/js/explore.js
+++ static/js/explore.js
@@ -231,9 +231,9 @@ async function handleSaveBook(book){
         body: JSON.stringify(savedBook)
     });
         const data = await response.json();
-        alert(data.message);
+        showSuccessPopup(data.message);
     } catch (err) {
-        alert('Save failed.');
+       showSuccessPopup('Save failed.');
     }
 
 };
@@ -329,3 +329,18 @@ document.addEventListener("click", () =>
 {
     card.classList.add("hidden");
 })
+
+
+
+
+
+function showSuccessPopup(message = "Book added to library ✔") {
+  const popup = document.getElementById("successPopup");
+  popup.textContent = message;
+
+  popup.classList.add("show");
+
+  setTimeout(() => {
+    popup.classList.remove("show");
+  }, 2000); 
+}
